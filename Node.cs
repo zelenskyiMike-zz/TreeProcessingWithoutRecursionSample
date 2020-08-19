a9feeb59a3c3213ee3c0a4ebc17b6ac5581ba66e
Mikhail Zelenskiy <zelenskiymo@gmail.com>
2020-08-20 00:43:00 +0300

    Tree processing without recursion algorythm.

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeProcessingWithoutRecursionSample
+{
+    public class Node
+    {
+        public List<Node> Childs;
+        public string NodeInfo;
+
+        public void AddChild(Node child)
+        {
+            Childs.Add(child);
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeProcessingWithoutRecursionSample
+{
+    public class Tree
+    {
+        private Node _node;
+       
+        public Tree(Node node)
+        {
+            this._node = node;
+        }
+
+        public Node SearchElement(string information)
+        {
+            if (_node.NodeInfo.Equals(information))
+            {
+                return _node;
+            }
+
+            var childs = this._node.Childs;
+            childs.Reverse();
+
+            var stack = new Stack<Node>(childs);
+
+            while (stack.Count > 0)
+            {
+                Node element = stack.Pop();
+
+                if (CheckNode(element, information, stack))
+                {
+                    return element;
+                }
+
+                var currentNodeChilds = element.Childs;
+                currentNodeChilds.Reverse();
+
+                foreach (var child in currentNodeChilds)
+                {
+                    stack.Push(child);
+                }
+            }
+            throw new KeyNotFoundException();
+        }
+
+        public bool CheckNode(Node child, string information, Stack<Node> stack)
+        {
+            if (!child.NodeInfo.Equals(information))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
